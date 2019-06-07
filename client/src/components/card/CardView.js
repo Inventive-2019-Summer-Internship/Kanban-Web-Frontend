f4dcfa3e56bbd96ead650ee5803d40c65bfad2a6
Unknown <ruben.s.ruiz@outlook.com>
2019-06-07 10:34:51 -0500

    drag and drop

--- client/src/components/card/CardView.js
+++ client/src/components/card/CardView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Cards from './Cards'
 import AddCard from './AddCard'
+import MoveCardPointer from './MoveCardPointer';
 
 
 export class CardView extends Component {
